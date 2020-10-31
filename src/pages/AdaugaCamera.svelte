a7ea0c7dc29d2973750c375d81e19b92ed32bd96
climentea <climente.alin@gmail.com>
2020-10-31 10:33:32 +0200

    linked list of checkbox to stores.js

--- src/pages/AdaugaCamera.svelte
+++ src/pages/AdaugaCamera.svelte
@@ -91,6 +91,7 @@ function saveRoom(event){
             <DropCheck name="Facilitati apartament" options={facilitati} field="facilitati"/>
             <DropCheck name="Cerinte chiriasi" options={cerinte} field="cerinte"/>
 
+
         </div>
 
         <button type="submit" class="table px-4 py-2 focus:outline-none outline-none text-xs lg:text-base rounded-md bg-blue-500 text-white">
