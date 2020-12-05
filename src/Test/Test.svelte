f12ea02c907945b494da9c6611984c7d6a59b23f
climentea <climente.alin@gmail.com>
2020-12-05 17:10:32 +0200

    stuck at fetching data from firestore (Test.svelte)

--- src/Test/Test.svelte
+++ src/Test/Test.svelte
@@ -1,17 +1,41 @@
 <script>
 
-export let message = "Info message"
-export let info = true
+import { db, fire } from "../Utils/fire.js"
+import { current_user } from "../Utils/auth.js"
+import Table from "../Widgets/Table/Table.svelte"
+
+
+// db = firebase.firestore()
+async function postedByUser(){
+    
+    let query = await db.collection("users").doc($current_user.uid).get()
+
+    let user_posts = []
+    const listingsRefs = await query.data().anunturi_postate 
+    
+    console.log(listingsRefs[0])
+
+    for (ref of listingsRefs) {
+        let post = await ref.get()
+        let post_data = await post.data() 
+        console.log(post_data) // data looks ok
+        user_posts.push(post_data)
+    }
+
+    // ref is not defined
+    console.log("user_posts:", user_posts) // here it's empty
+    return user_posts
+}
+
+
+let user_posts 
+postedByUser().then(data => {
+    user_posts = data
+})
+
+// here it's empty
+console.log("After func call: ", user_posts)
+
 
 </script>
-
-
-<div 
-    class:border-green-500={info} class:border-red-500={!info} 
-    class="absolute bg-blue-900 border-l-8 bottom-0 left-0 mb-4 ml-4 p-2 pr-4 rounded-r-md shadow-md text-right text-white w-48 z-20"
->
-    
-{message}
-
-</div>
 
---
+++ src/Widgets/Table/Table.svelte
@@ -0,0 +1,89 @@
+<script>
+
+export let data
+
+
+let columns = []
+let values = []
+for (const [col, val] of Object.entries(data)) {  
+  if (Array.isArray(val)) {
+    columns.push(col)
+    values.push(val)
+  }
+}
+
+// console.log(columns)
+// console.log(values)
+
+
+// From the list of values get the max len of a list 
+let rows_len = Math.max(0, ...values.map(item => item.length))
+
+// Normalize lists to have the same length as the biggest one in the nested lists
+let rows = values.map(li => {
+    if (li.length != rows_len) {
+        let fill_arr = Array.from({length: rows_len - li.length}).map(el => "")
+        li.push(...fill_arr)
+    }
+    return li
+})
+
+
+// console.log(rows)
+
+
+// Make 2 dummy lists with the length of cols and rows
+let cols_range = Array.from({length: columns.length}).map(el => "")
+let rows_range = Array.from({length: rows_len}).map(el => "")
+
+
+// console.log(`${rows_range.length}rows, ${cols_range.length}cols`)
+
+
+let html_rows = []
+for (let ir = 0; ir < rows_range.length; ir++) {
+
+    let html_row = []
+    for (let ic = 0; ic < cols_range.length; ic++) {
+        // console.log(ic, ir, rows[ic][ir])
+        html_row.push(rows[ic][ir])
+    }
+
+    html_rows.push(html_row)
+}
+
+// console.log(html_rows)
+
+</script>
+
+<div class="overflow-x-auto p-2 text-xs md:text-sm">
+
+<table class="w-full mt-4">
+    <thead class="capitalize border-b-2 font-semibold">
+        <tr>
+            {#each columns as col}
+                <td>{col}</td>
+            {/each}
+        </tr>
+    </thead>
+
+    <tbody class="text-gray-800">
+        {#each html_rows as row}
+          <tr>
+            {#each row as cell}
+              <td>{cell}</td>
+            {/each}
+          </tr>
+        {/each}
+    </tbody>
+</table>
+
+</div>
+
+
+
+<style>
+table {
+    min-width: 470px;
+}
+</style>
