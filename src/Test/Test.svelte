8f99f6d4bc0d7a99cbf9fd1a0fbde31fc25d6540
climentea <climente.alin@gmail.com>
2020-12-05 17:37:46 +0200

    forgot a to define the var in the for loop

--- src/Test/Test.svelte
+++ src/Test/Test.svelte
@@ -5,25 +5,18 @@ import { current_user } from "../Utils/auth.js"
 import Table from "../Widgets/Table/Table.svelte"
 
 
-// db = firebase.firestore()
 async function postedByUser(){
     
     let query = await db.collection("users").doc($current_user.uid).get()
 
     let user_posts = []
-    const listingsRefs = await query.data().anunturi_postate 
-    
-    console.log(listingsRefs[0])
-
-    for (ref of listingsRefs) {
+    const listingsRefs = query.data().anunturi_postate 
+    for (let ref of listingsRefs) {
         let post = await ref.get()
         let post_data = await post.data() 
-        console.log(post_data) // data looks ok
         user_posts.push(post_data)
     }
 
-    // ref is not defined
-    console.log("user_posts:", user_posts) // here it's empty
     return user_posts
 }
 
@@ -33,8 +26,8 @@ postedByUser().then(data => {
     user_posts = data
 })
 
-// here it's empty
-console.log("After func call: ", user_posts)
+
+$: console.log("Results: ", user_posts)
 
 
 </script>
